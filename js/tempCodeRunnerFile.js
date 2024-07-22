b7466f21c148e4fe307b0e4f991dfc4beaacf501
emersonfsti <emersonfsti@gmail.com>
2024-07-21 23:26:00 -0300

    commit20240721001

--- js/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-let palavraGerada =  combinandoNomesPokemons("Bulba");
-console.log(palavraGerada);
-
-function combinandoNomesPokemons(palavra) {
-  let palavraPokemon = palavra + "saur";
-  return pokemon;
-}
